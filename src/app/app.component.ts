ef5c801902141b2a0ad510f427856ccdb83b482a
Sandeep Reddy <sandeepkumar@zoof.co.in>
2024-11-06 12:42:13 +0530

    USERS Manipilyutaion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { GL_INFO } from './model/general-ledger.model';
 export class AppComponent {
   title = 'angular-array-manipulations';
   glData : any = GL_INFO;
-
+  usersInfo: any[] = [];
 
   constructor() {
     const information = JSON.parse(JSON.stringify(this.glData));
@@ -18,6 +18,44 @@ export class AppComponent {
 
     fetch('https://dummyjson.com/users')
     .then(res => res.json())
-    .then(console.log);
+    .then(res => {
+      this.usersInfo = res?.users;
+      
+      const users = JSON.parse(JSON.stringify(this.usersInfo));
+      console.log("USERS INFO",users);
+      // users?.forEach((user: any,index: any) => {
+      // //   const sc = user?.company?.address?.stateCode;
+      // //   // console.log("User OBject For the Index",index,sc);
+      // //   const stateCodeIndex = ["MS", "WI", "CA"].indexOf(sc);
+      // //   // console.log("Index Value of the Ssate Code",stateCodeIndex);
+
+      // //   if(stateCodeIndex >= 0) {
+      // //     console.log("State Code is Present in Given List",user)
+      // //   } 
+      // //   // else {
+      // //   //   console.log("State Code is Not Present in the Given List");
+      // //   // }
+      //   const homeAddress = user?.address?.city
+      //   const officeAddress = user?.company?.address?.city;
+        
+      // });
+
+      const updatedUsers = JSON.parse(JSON.stringify(this.usersInfo))?.sort((a:any,b: any) =>  b?.age - a?.age);
+
+      console.log("TRANSFORMED USERS ",updatedUsers)
+
+
+    //   const hasSelectedCity = users.some((user: any,index: any) => {
+    //     const homeAddress = user?.address?.city
+    //     const officeAddress = user?.company?.address?.city;
+    //     if(homeAddress === 'India' || officeAddress === 'India') {
+    //      return true
+    //     }
+    //     return false
+    //   });
+
+    //   console.log("hasSelectedCity",hasSelectedCity)
+    });
+  
   }
 }
